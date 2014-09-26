76b6b8d7a84a968eefb1f34fce8ecc8c2196bf84
Caleb Sander <csander@2B7-iMac.local>
2014-09-26 08:59:36 -0400

    Commenting

--- Interest.java
+++ Interest.java
@@ -9,14 +9,14 @@ import java.util.Scanner;
 class Interest {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Annual rate (%): ");
-    double rate = scanner.nextDouble() / 100;
-    System.out.println("Maximum rate: " + ((Math.pow(Math.E, rate) - 1) * 100) + "%");
-    long input = 1;
-    System.out.print("Number of compounds per year: ");
-    while ((input = scanner.nextLong()) != 0) {
-      System.out.println("Rate: " + ((Math.pow(rate / input + 1, input) - 1) * 100) + "%");
-      System.out.print("Number of compounds per year: ");
+    System.out.print("Annual rate (%): "); //prompt for rate input
+    double rate = scanner.nextDouble() / 100; //convert percent to decimal
+    System.out.println("Maximum compounded annual rate: " + ((Math.pow(Math.E, rate) - 1) * 100) + "%"); //do e^r calculation and convert back to percent
+    long input;
+    System.out.print("Number of compounds per year: "); //prompt for compounds input
+    while ((input = scanner.nextLong()) != 0) { //keep doing calculations until a value of 0 is inputted
+      System.out.println("Compounded annual rate: " + ((Math.pow(rate / input + 1, input) - 1) * 100) + "%"); //do compounding calculation and convert back to percent
+      System.out.print("Number of compounds per year: "); //prompt for compounds input
     }
   }
 }
