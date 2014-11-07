acb1c2af38514f261120c17a13e3d9ecc2c99132
Caleb Sander <csander@2B16-iMac.local>
2014-11-07 08:49:58 -0500

    More programming of Hangman

--- Hangman.java
+++ Hangman.java
@@ -9,9 +9,11 @@ import java.util.ArrayList;
 
 class Hangman {
   public static void main(String[] args) {
+    clearScreen();
     Scanner scanner = new Scanner(System.in);
     System.out.print("What word would you like to use? "); //prompt for word input
     String word = scanner.next();
+    clearScreen(); //so the word can't be seen by the opponent
     char[] guessedword = new char[word.length()]; //stores which characters in the word have been guessed so far
     for (int i = 0; i < word.length(); i++) guessedword[i] = '_'; //fill guessed "string" with underscores (to show that no characters have yet been guessed)
     ArrayList<Character> guesses = new ArrayList<Character>(); //stores past guesses
@@ -22,11 +24,11 @@ class Hangman {
       System.out.print("Guess a letter! "); //prompt for letter input
       guess = scanner.next().toCharArray()[0]; //get single character from input
       clearScreen();
-      if (guesses.indexOf(guess) == -1) { //if character hasn't yet been guessed
+      if (guesses.indexOf(guess) == -1) { //if letter hasn't yet been guessed
         guesses.add(guess); //record that character has been guessed
-        indices = indicesOf(word, guess);
+        indices = indicesOf(word, guess); //find occurences of the letter in the word
         if (indices.size() == 0) { //if the guess is not in the word
-          guessesremaining--;
+          guessesremaining--; //count down remaining guesses
           System.out.println("You guessed a letter that is not in the word.");
           printHangman(guessesremaining);
         }
@@ -34,14 +36,14 @@ class Hangman {
           for (int i = 0; i < indices.size(); i++) guessedword[indices.get(i).intValue()] = guess; //replace underscores with guessed character, where they should be
           System.out.println("You guessed a letter that is in the word: " + new String(guessedword));
         }
-        String lettersstring = ""; //a space-separated list of all the old guesses, to print later
-        for (int i = 0; i < guesses.size(); i++) {
+        String lettersstring = ""; //a space-separated list of all the old guesses to tell the user what they can guess
+        for (int i = 0; i < guesses.size(); i++) { //iterate through each of the old guesses
           char[] oldguess = {guesses.get(i)}; //because characters can't be converted to strings
-          lettersstring += new String(oldguess) + " ";
+          lettersstring += new String(oldguess) + " "; //add the guess and a space character to the string
         }
         System.out.println(lettersstring); //print the list of previously guessed letters
       }
-      else System.out.println("Already guessed " + guess + ". Please guess something else.");
+      else System.out.println("Already guessed \"" + guess + "\". Please guess something else."); //if the letter has been guessed, then ask for another
     }
     if (new String(guessedword).equals(word)) System.out.println("You WON!"); //if the word was correctly guessed, display so
     else System.out.println("You LOST!"); //if the word wasn't guessed, display so
@@ -50,14 +52,15 @@ class Hangman {
 
   public static ArrayList<Integer> indicesOf(String word, char guess) { //returns an array of undetermined length of integer indices for a character in the word
     ArrayList<Integer> indices = new ArrayList<Integer>();
-    for (int i = 0; i < word.length(); i++) {
-      if (word.charAt(i) == guess) indices.add(i);
+    for (int i = 0; i < word.length(); i++) { //iterate over the word, character-by-character
+      if (word.charAt(i) == guess) indices.add(i); //keep a list of the occurences
     }
     return indices;
   }
 
   public static void printHangman(byte guessesremaining) { //prints ASCII art corresponding to the number of turns remaining
-    System.out.println(guessesremaining + " guesses remaining:");
+    if (guessesremaining == 1) System.out.println("1 guess remaining:"); //don't pluralize "guess" if only one remains
+    else System.out.println(guessesremaining + " guesses remaining:"); //otherwise, just print out how many guesses remain
     switch (guessesremaining) {
       case 0:
         System.out.println("________ ");
@@ -123,7 +126,7 @@ class Hangman {
         System.out.println("---------");
     }
   }
-  public static void clearScreen() { //prints a bunch of newlines to effectively clear the screen
-    System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+  public static void clearScreen() { //prints a bunch of line feeds to effectively clear the screen
+    System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
   }
 }
