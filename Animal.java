a2daf65c9ea0fc4452407369deff5369d9488246
Caleb Sander <potatoceros@gmail.com>
2014-12-06 22:39:08 -0500

    Fixed error, added comments

--- Animal.java
+++ Animal.java
@@ -8,28 +8,28 @@ import java.util.ArrayList;
 
 class Rabbit {
 	//Attributes
-	protected ArrayList<String> vegetablesEaten;
-	private double distanceHopped;
-	protected double health;
-	private double food;
-	private double age;
+	protected ArrayList<String> vegetablesEaten; //stores all vegetables eaten
+	private double distanceHopped; //stores current position
+	protected double health; //stores current health
+	private double food; //stores current food level
+	private double age; //stores current age
 
-	//Constructor
-	Rabbit() {
+	//Constructors
+	Rabbit() { //constructs a default rabbit
 		this.vegetablesEaten = new ArrayList<String>();
 		this.distanceHopped = 0.0;
 		this.health = 1.0;
 		this.food = 0.5;
 		this.age = 0.0;
 	}
-	Rabbit(double health) {
+	Rabbit(double health) { //constructs a rabbit with specified health
 		this.vegetablesEaten = new ArrayList<String>();
 		this.distanceHopped = 0.0;
 		this.health = health;
 		this.food = 0.5;
 		this.age = 0.0;
 	}
-	Rabbit(double health, double food) {
+	Rabbit(double health, double food) { //constructs a rabbit with specified health and food level
 		this.vegetablesEaten = new ArrayList<String>();
 		this.distanceHopped = 0.0;
 		this.health = health;
@@ -38,38 +38,38 @@ class Rabbit {
 	}
 
 	//Methods
-	public boolean alive() {
+	public boolean alive() { //returns whether the rabbit is alive
 		return this.health > 0.0;
 	}
-	public double eat(String food) {
+	public double eat(String food) { //adds food to list of foods eaten, increases foodcount depending on length of food string (only works if rabbit is at least partially alive); returns food level
 		if (!alive()) return this.food;
 		this.vegetablesEaten.add(food);
-		return this.food = Math.min(this.food + food.length() / 20.0, 1.0);
+		return this.food = Math.min(this.food + food.length() / 20.0, 1.0); //don't overfill food count
 	}
-	public boolean hungry() {
+	public boolean hungry() { //returns whether the rabbit is hungry
 		return this.food < 0.2;
 	}
-	public void hopForward(double distance) {
+	public void hopForward(double distance) { //moves the rabbit and uses up food (only works if rabbit is at least partially alive)
 		if (!alive()) return;
 		this.distanceHopped += distance;
 		this.food = Math.max(this.food - Math.abs(distance), 0.0);
 	}
-	public void hopBackward(double distance) {
+	public void hopBackward(double distance) { //hops the other way
 		hopForward(-distance);
 	}
-	public double position() {
+	public double position() { //returns current position (after hopping)
 		return this.distanceHopped;
 	}
-	public double age() {
+	public double age() { //increments the rabbit's age by one year; returns the rabbit's new age
 		return ++this.age;
 	}
-	public double age(double years) {
+	public double age(double years) { //increments the rabbit's age by any number of years; returns the rabbit's new age
 		return this.age += years;
 	}
-	public void heal() {
+	public void heal() { //restores health to full (only works if rabbit is at least partially alive)
 		if (alive()) this.health = 1.0;
 	}
-	public double damage(double amount) {
+	public double damage(double amount) { //decreases the rabbit's health, but not below 0; returns the new health
 		return this.health = Math.max(this.health - amount, 0.0);
 	}
 }
