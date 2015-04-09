af6834e66ecdb114ccf280327ac9e37adbe5491f
Caleb Sander <caleb.sander@gmail.com>
2015-04-08 20:28:18 -0400

    Added file-reading ability, work on new method that needs testing

--- Sudoku/Run.java
+++ Sudoku/Run.java
@@ -1,22 +1,48 @@
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.IOException;
 
 class Run {
 	public static void main(String[] args) {
-		System.out.println("Enter puzzle:");
-		Scanner scanner = new Scanner(System.in);
-		scanner.useDelimiter("\n");
-		String[] rowStrings = new String[9];
-		for (int i = 0; i < 9; i++) rowStrings[i] = scanner.next();
-		scanner.close();
-		Sudoku sudoku = new Sudoku(rowStrings);
-		Sudoku lastSudoku = new Sudoku(rowStrings);
-		do {
-			lastSudoku = sudoku.clone();
-			sudoku.killPossibilities();
-			sudoku.chooseUnique();
-			sudoku.findContainedGroups();
-		} while (!sudoku.equals(lastSudoku));
-		sudoku.print();
+		if (args.length != 1) {
+			System.out.println("Correct usage:");
+			System.out.println("\tjava Run puzzles.txt");
+			return;
+		}
+		try {
+			String[] rowStrings;
+			char[] tempCharArray;
+			Sudoku sudoku, lastSudoku;
+			FileReader inputStream = new FileReader(args[0]);
+			inputStream.read();
+			for (int i = 0, j, k; i == 0 || inputStream.read() != ']'; i++) { //iterate over puzzles
+				inputStream.read();
+				rowStrings = new String[9];
+				for (j = 0; j < 9; j++) { //iterate over rows
+					rowStrings[j] = "";
+					inputStream.read();
+					for (k = 0; k < 9; k++) { //iterate over squares
+						tempCharArray = new char[1];
+						tempCharArray[0] = (char)inputStream.read();
+						rowStrings[j] += new String(tempCharArray);
+						inputStream.read();
+					}
+					inputStream.read();
+				}
+				sudoku = new Sudoku(rowStrings);
+				lastSudoku = new Sudoku(rowStrings);
+				do {
+					lastSudoku = sudoku.clone();
+					sudoku.killPossibilities();
+					sudoku.chooseUnique();
+					sudoku.findContainedGroups();
+				} while (!sudoku.equals(lastSudoku));
+				sudoku.print();
+				System.out.println();
+			}
+			inputStream.close();
+		}
+		catch (IOException e) {
+			System.out.println("Error reading file");
+		}
 	}
 }
