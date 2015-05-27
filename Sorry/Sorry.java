4d897eb17a1c2e8657cfadfd9d57d0bf5bc835e4
Caleb Sander <caleb.sander@gmail.com>
2015-05-26 22:13:20 -0400

    Minor name changes

--- Sorry/Sorry.java
+++ Sorry/Sorry.java
@@ -1,13 +1,14 @@
 import javax.swing.JFrame;
 
 class Sorry {
-	private final static int WIDTH = 768;
+	private final static int WINDOW_SIZE = 768;
+
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Sorry!");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		frame.add(new Board(WIDTH));
-		frame.setSize(WIDTH + 20, WIDTH + 50);
+		frame.add(new Board(WINDOW_SIZE));
+		frame.setSize(WINDOW_SIZE + 20, WINDOW_SIZE + 50);
 		frame.setVisible(true);
 	}
 }
