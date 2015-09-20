8c6b3b5b36443e9c3a280de3fd9e46de06429fbe
Caleb Sander <caleb.sander@gmail.com>
2015-09-19 21:17:50 -0400

    Better comments and layout, and a program to ensure that no lines are too long

--- 3/Sander_C2HTML.c
+++ 3/Sander_C2HTML.c
@@ -1,14 +1,29 @@
+/*C2HTML
+	Caleb Sander
+	09/15/2015
+	Takes in C programs and outputs them with HTML formatting
+*/
+
 #include <stdbool.h>
 #include <stdio.h>
 
-bool gInBlockComment = false; //whether a block comment has started but not yet finished at the current posititon in the input
-bool gInLineComment = false; //whether a line comment has started but not yet finished
-bool gInString = false; //whether a string literal has started but not yet finished
-bool gInCharacter = false; //whether a character literal has started but not yet finished
+/*Whether a block comment has started but not yet finished at the current
+	posititon in the input*/
+bool gInBlockComment = false;
+//Whether a line comment has started but not yet finished
+bool gInLineComment = false;
+//Whether a string literal has started but not yet finished
+bool gInString = false;
+//Whether a character literal has started but not yet finished
+bool gInCharacter = false;
 
+/*Takes in the next input char and prints the necessary HTML formatting and
+	updates the global state variables if necessary
+	inputChar is the char to process*/
 void process(char inputChar) {
-	char nextChar;
-	switch (inputChar) { //go through the different cases that need special attention
+	char nextChar; //used when checking for two-character sequences
+	//Go through the different cases that need special attention
+	switch (inputChar) {
 		case '<': //must be escaped in HTML
 			printf("&lt;");
 			break;
@@ -19,11 +34,13 @@ void process(char inputChar) {
 			printf("&amp;");
 			break;
 		case '\\':
+			//Skip checking the next character because it is escaped
 			putchar(inputChar);
-			putchar(getchar()); //skip checking the next character because it is escaped
+			putchar(getchar());
 			break;
-		case '"': //start or end of string (if not nested in a character)
-			if (gInCharacter) putchar(inputChar); //if in '"', then just print it normally
+		case '"': //start or end of string (if not nested in comment or character)
+			//If in '"', then just print it normally
+			if (gInCharacter || gInBlockComment || gInLineComment) putchar(inputChar);
 			else {
 				gInString = !gInString; //we have either started or stopped a string
 				if (gInString) {
@@ -31,26 +48,32 @@ void process(char inputChar) {
 					putchar(inputChar);
 				}
 				else {
-					putchar(inputChar); //make sure quotation mark goes inside the <b> tags
+					putchar(inputChar);
 					printf("</b>");
 				}
 			}
 			break;
 		case '\'': //start or end of character (if not nested in a string)
 			putchar(inputChar);
-			if (!gInString) gInCharacter = !gInCharacter; //only start/stop a character literal if not inside a string
+			//Only start/stop a character literal if not inside a string or comment
+			if (!(gInString || gInBlockComment || gInLineComment))
+				gInCharacter = !gInCharacter;
 			break;
 		case '/': //potentially the start of a comment
-			nextChar = getchar(); //we need a second character in order to be able to decide
+			//We need a second character in order to be able to decide if it is a comment
+			nextChar = getchar();
 			if (nextChar == '/') { //potential start of line comment
-				if (!gInLineComment) { //can't do another line comment on the same line
+				//Can't be nested
+				if (!(gInBlockComment || gInLineComment || gInCharacter || gInString)) {
 					printf("<i>");
 					gInLineComment = true;
 				}
 				putchar(inputChar);
 				putchar(nextChar);
 			}
-			else if (nextChar == '*' && !gInBlockComment) { //if we are already in a block comment, make sure to check to see whether the '*' is part of the end signature (process again)
+			else if (nextChar == '*' &&
+			//Can't be nested
+			!(gInBlockComment || gInLineComment || gInCharacter || gInString)) {
 				printf("<i>");
 				putchar(inputChar);
 				putchar(nextChar);
@@ -58,18 +81,22 @@ void process(char inputChar) {
 			}
 			else {
 				putchar(inputChar); //print the slash normally
-				process(nextChar); //the next character could conceivably also matter for the formatting
+				/*if the next character wasn't special or we are already in a block comment,
+					make sure to check to see whether the '*' needs special processing*/
+				process(nextChar);
 			}
 			break;
 		case '*': //potentially the end of a block comment
 			nextChar = getchar();
-			if (nextChar == '/' && gInBlockComment) { //gInBlockComment should never be false when this happens, but try to handle bad entry gracefully (no unmatched </i>)
+			/*gInBlockComment should never be false when this happens,
+				but try to handle bad entry gracefully (no unmatched </i>)*/
+			if (nextChar == '/' && gInBlockComment) {
 				putchar(inputChar);
 				putchar(nextChar);
 				printf("</i>");
 				gInBlockComment = false;
 			}
-			else {
+			else { //just a normal next character
 				putchar(inputChar);
 				process(nextChar); //see else block in case '/'
 			}
@@ -88,7 +115,8 @@ void process(char inputChar) {
 
 int main() {
 	printf("<pre>\n"); //wrap the code in <pre> tags
+	//Iterate through input stream until finding an EOF character
 	char inputChar;
-	while ((inputChar = getchar()) != EOF) process(inputChar); //iterate through stream until finding an EOF character
-	printf("</pre>\n");
+	while ((inputChar = getchar()) != EOF) process(inputChar);
+	printf("</pre><!--By the way, the 80 character line limit is annoying.-->\n");
 }
