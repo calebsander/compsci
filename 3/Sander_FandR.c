bd6cf9adb56c83f7e32a3d29cc6053aa6de40bba
Caleb Sander <caleb.sander@gmail.com>
2015-11-03 20:22:11 -0500

    Logic for switching between rules, fixed memory leaks

--- 3/Sander_FandR.c
+++ 3/Sander_FandR.c
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,7 +23,9 @@ void allocateExtra(GrowableString *string) {
 	}
 }
 GrowableString *emptyString() {
-	GrowableString *string = calloc(sizeof(*string), 1);
+	GrowableString *string = malloc(sizeof(*string) * 1);
+	string->string = NULL; //signal that it needs to be allocated
+	string->length = 0;
 	string->allocatedLength = 16;
 	allocateCharacters(string);
 	*(string->string) = '\0';
@@ -38,6 +39,10 @@ GrowableString *newStringFromMallocd(char *mallocdString) {
 	string->allocatedLength = length + 1;
 	return string;
 }
+void freeString(GrowableString *string) {
+	free(string->string);
+	free(string);
+}
 
 void concat(GrowableString *onto, char from) {
 	char *insertionIndex = onto->string + onto->length;
@@ -47,20 +52,18 @@ void concat(GrowableString *onto, char from) {
 	*(insertionIndex + 1) = '\0';
 }
 void insertAt(GrowableString *string, unsigned int index, unsigned int deleteLength, char *insertString) {
-	unsigned int insertLength = strlen(insertString);
+	const unsigned int insertLength = strlen(insertString);
 	string->length += insertLength;
 	string->length -= deleteLength;
 	allocateExtra(string);
-	unsigned int indexAfterInsertion = index + insertLength;
-	memcpy(string->string + indexAfterInsertion, string->string + index + deleteLength, string->length + 1 - indexAfterInsertion); //copy null byte too
+	const unsigned int indexAfterInsertion = index + insertLength;
+	//Correct these to account for actual sizeof(char)
+	memmove(string->string + indexAfterInsertion, string->string + index + deleteLength, (string->length + 1 - indexAfterInsertion) * sizeof(char)); //copy null byte too
+	memcpy(string->string + index, insertString, insertLength * sizeof(char));
 }
 
 typedef enum {
-	QUIT,
-	NEXT,
-	RESCAN,
-	START,
-	INVALID
+	QUIT, NEXT, RESCAN, START, INVALID
 } Flag;
 typedef struct {
 	char *from;
@@ -73,11 +76,12 @@ typedef struct {
 
 ReplacementRule *parseRule(char *fromString, char *toString) {
 	ReplacementRule *rule = malloc(sizeof(*rule));
-	if (fromString[0] == '^') {
+	if (*fromString == '^') {
 		rule->atStart = true;
 		fromString++;
 	}
 	else rule->atStart = false;
+	rule->atEnd = false;
 	GrowableString *normalizedFromString = emptyString();
 	char nextChar;
 	for (; *fromString; fromString++) {
@@ -92,11 +96,7 @@ ReplacementRule *parseRule(char *fromString, char *toString) {
 				break;
 			case '#':
 				nextChar = *(fromString + 1);
-				if (nextChar) {
-					concat(normalizedFromString, *fromString);
-					concat(normalizedFromString, nextChar);
-					fromString++;
-				}
+				if (nextChar) concat(normalizedFromString, *fromString);
 				else rule->atEnd = true;
 				break;
 			default:
@@ -104,8 +104,13 @@ ReplacementRule *parseRule(char *fromString, char *toString) {
 		}
 	}
 	rule->from = normalizedFromString->string;
+	free(normalizedFromString);
 	rule->to = toString;
 	return rule;
+}
+void freeRule(ReplacementRule *rule) {
+	free(rule->from);
+	free(rule);
 }
 const char *CORRECT_SYNTAX = "Syntax:\t./FandR [-SRNQsrnq] [FROM TO]*\n";
 void argumentError() {
@@ -148,49 +153,66 @@ Flags parseFlags(char *string) {
 	}
 	return flags;
 }
+unsigned int getNextSearchIndex(Flag flag, unsigned int insertionIndex, unsigned int insertionLength) {
+	switch (flag) {
+		case NEXT:
+			return insertionIndex + insertionLength;
+		case RESCAN:
+			return insertionIndex;
+		case START:
+			return 0;
+		default:
+			return 0; //doesn't matter for QUIT or INVALID
+	}
+}
 bool runReplacement(char *line, ReplacementRule *rule, Flag flag) { //returns success
 	unsigned int index = 0;
 	bool changedLastTime = true;
 	bool anchorConditionsMet;
 	bool success = false;
 	const unsigned int fromLength = strlen(rule->from);
+	const unsigned int insertionLength = strlen(rule->to);
 	GrowableString *wrappedLine = newStringFromMallocd(line);
 	while (changedLastTime) {
 		anchorConditionsMet = true;
-		if (rule->atStart && !strncmp(rule->from, line, fromLength)) anchorConditionsMet = false;
-		else if (rule->atEnd && !strncmp(rule->from, line + strlen(line) - fromLength, fromLength)) anchorConditionsMet = false;
+		if (rule->atStart && !strncmp(rule->from, wrappedLine->string, fromLength)) anchorConditionsMet = false;
+		else if (rule->atEnd && !strncmp(rule->from, wrappedLine->string + strlen(wrappedLine->string) - fromLength, fromLength)) anchorConditionsMet = false;
+		printf("Anchor: %d\n", anchorConditionsMet);
 		if (anchorConditionsMet) {
-			char *foundIndex = strstr(line + index, rule->from);
+			char *foundIndex = strstr(wrappedLine->string + index, rule->from);
+			printf("Index: %d\n", (int)(foundIndex ? foundIndex - wrappedLine->string : -1));
 			if (foundIndex) {
-				insertAt(wrappedLine, foundIndex - line, fromLength, rule->to);
-				changedLastTime = true;
-				switch (flag) {
-					case QUIT:
-						changedLastTime = false;
-						break;
-					case NEXT:
-						index = foundIndex - line + strlen(rule->to);
-						break;
-					case RESCAN:
-						index = foundIndex - line;
-						break;
-					case START:
-						index = 0;
-						break;
-					case INVALID:
-						assert(false);
-				}
+				unsigned int insertionIndex = foundIndex - wrappedLine->string;
+				insertAt(wrappedLine, insertionIndex, fromLength, rule->to);
+				changedLastTime = flag != QUIT;
+				success = true;
+				index = getNextSearchIndex(flag, insertionIndex, insertionLength);
 			}
 			else changedLastTime = false;
 		}
 		else changedLastTime = false;
-		if (changedLastTime) success = true;
 	}
+	free(wrappedLine);
 	return success;
+}
+unsigned int getNextRuleIndex(Flag flag, unsigned int index, bool success) {
+	switch (flag) {
+		case NEXT:
+			if (success) return index + 1;
+			else return index;
+		case RESCAN:
+			if (success) return index;
+			else return index + 1;
+		case START:
+			if (success) return 0;
+			else return index + 1;
+		default:
+			return 0; //doesn't matter for QUIT or INVALID
+	}
 }
 
 int main(int argc, char **argv) {
-	Flags flags;
+	Flags flags = {NEXT, NEXT};
 	argc -= 1;
 	argv++;
 	if (argc % 2) { //options flag
@@ -199,10 +221,25 @@ int main(int argc, char **argv) {
 		if (flags.rule == INVALID) flags.rule = NEXT;
 		argv++;
 	}
-	else flags.meta = flags.rule = NEXT; //no options flag
-	ReplacementRule **rules = malloc(sizeof(*rules) * (argc / 2));
+	unsigned int numRules = (argc / 2);
+	ReplacementRule **rules = malloc(sizeof(*rules) * numRules);
 	for (unsigned int i = 0; argv[i * 2]; i++) {
 		rules[i] = parseRule(argv[i * 2], argv[i * 2 + 1]);
-		printf("%s, %s\n", rules[i]->from, rules[i]->to);
+		printf("Rule: %s, %s, %d, %d\n", rules[i]->from, rules[i]->to, rules[i]->atStart, rules[i]->atEnd);
 	}
+	char *origLine = "aabbab";
+	char *line = malloc(strlen(origLine) + 1);
+	strcpy(line, origLine);
+	unsigned int index = 0;
+	bool changedLastTime = false;
+	while (index != numRules && !(changedLastTime && flags.meta == QUIT)) {
+		printf("\nRule Index: %u\n", index);
+		changedLastTime = runReplacement(line, rules[index], flags.rule);
+		index = getNextRuleIndex(flags.meta, index, changedLastTime);
+		printf("Changed: %d ", changedLastTime);
+		printf("Replaced: %s\n", line);
+	}
+	free(line);
+	for (unsigned int i = 0; i < numRules; i++) freeRule(rules[i]);
+	free(rules);
 }
