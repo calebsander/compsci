02bd01c3e86a9abb84ad5aa1718bb38ccd02d460
Caleb Sander <caleb.sander@gmail.com>
2015-12-02 09:55:08 -0500

    Made addition and subtraction of indices into separate functions

--- 3/deque/deque.c
+++ 3/deque/deque.c
@@ -10,7 +10,7 @@ struct deque {
 };
 /*
 	Stored elements begin at index start and finish at index end,
-	wrapping around if end < start
+	wrapping around if end < start; can store at most DEFAULT_SIZE - 1 elements
 */
 
 Deque *makeEmptyDeque() {
@@ -20,16 +20,22 @@ Deque *makeEmptyDeque() {
 	return deque;
 }
 
+void addWrap(unsigned int *value) {
+	*value = (*value + 1) % DEFAULT_SIZE;
+}
+void subtractWrap(unsigned int *value) {
+	*value = (*value + DEFAULT_SIZE - 1) % DEFAULT_SIZE; //make sure we don't have overflow and correct for % not really doing modulus
+}
 bool isEmptyDeque(Deque *deque) {
 	return deque->start == deque->end;
 }
 void pushFront(int element, Deque *deque) {
-	deque->start = (deque->start + DEFAULT_SIZE - 1) % DEFAULT_SIZE;
+	subtractWrap(&(deque->start));
 	deque->elements[deque->start] = element;
 }
 void pushBack(int element, Deque *deque) {
 	deque->elements[deque->end] = element;
-	deque->end = (deque->end + 1) % DEFAULT_SIZE;
+	addWrap(&(deque->end));
 }
 void errorIfEmpty(Deque *deque) {
 	if (isEmptyDeque(deque)) {
@@ -47,12 +53,12 @@ int peekBack(Deque *deque) {
 }
 int popFront(Deque *deque) {
 	const int value = peekFront(deque);
-	deque->start = (deque->start + 1) % DEFAULT_SIZE;
+	addWrap(&(deque->start));
 	return value;
 }
 int popBack(Deque *deque) {
 	const int value = peekBack(deque);
-	deque->end = (deque->end + DEFAULT_SIZE - 1) % DEFAULT_SIZE;
+	subtractWrap(&(deque->end));
 	return value;
 }
 void freeDeque(Deque *deque) {
